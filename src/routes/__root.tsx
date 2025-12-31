023416d256c26f33b44087b04487578528e8060e
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-12-31 12:23:38 -0800

    add vercel analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Analytics } from '@vercel/analytics/react'
 
 import appCss from '../styles.css?url'
 
@@ -15,7 +16,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Shader Studio',
       },
     ],
     links: [
@@ -49,6 +50,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           ]}
         />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   )
